3531a7fa2e41c3f12337a37da7b96d2fd9972413
Roddy <rlm72@cam.ac.uk>
2016-12-02 13:14:43 +0000

    Corrected Enemy classes

--- Game/Enemy.cpp
+++ Game/Enemy.cpp
@@ -1,10 +1,10 @@
 #include "stdafx.h"
 #include "Enemy.h"
 
-Player::Player(ResourceManager<sf::Texture, std::string>* resourceManager, EntityManager* entityManager) {
-    constructEntity(resourceManager, "test1", entityManager, sfld::Vector2f(50, 50), Entity::SHAPE_SQUARE, Entity::DYNAMIC_MOVING, Entity::TYPE_DEFAULT);
+Enemy::Enemy(ResourceManager<sf::Texture, std::string>* resourceManager, EntityManager* entityManager, int initial_x, int initial_y) {
+    constructEntity(resourceManager, "enemy", entityManager, sfld::Vector2f(initial_x, initial_y), Entity::SHAPE_SQUARE, Entity::DYNAMIC_MOVING, Entity::TYPE_DEFAULT);
 }
 
-void Player::update(int frame_time) {
+void Enemy::update(int frame_time) {
 
 }
