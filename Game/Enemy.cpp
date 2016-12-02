2953d6a9865f87d25fef2cf10a702faec45f166b
Roddy <rlm72@cam.ac.uk>
2016-12-02 13:11:33 +0000

    Filled in Enemy classes

--- Game/Enemy.cpp
+++ Game/Enemy.cpp
@@ -1,5 +1,10 @@
-//
-// Created by Roddy MacSween on 02/12/2016.
-//
-
+#include "stdafx.h"
 #include "Enemy.h"
+
+Player::Player(ResourceManager<sf::Texture, std::string>* resourceManager, EntityManager* entityManager) {
+    constructEntity(resourceManager, "test1", entityManager, sfld::Vector2f(50, 50), Entity::SHAPE_SQUARE, Entity::DYNAMIC_MOVING, Entity::TYPE_DEFAULT);
+}
+
+void Player::update(int frame_time) {
+
+}
--- Game/Enemy.h
+++ Game/Enemy.h
@@ -1,4 +1,10 @@
-#ifndef GAMEGIG_ENEMY_H
-#define GAMEGIG_ENEMY_H
+#pragma once
 
-#endif //GAMEGIG_ENEMY_H
+#include "Entity.h"
+
+class Enemy : public Entity {
+public:
+    Enemy(ResourceManager<sf::Texture, std::string>* resourceManager, EntityManager* entityManager);
+
+    void update(int frame_time);
+};
