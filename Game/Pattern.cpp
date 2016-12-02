6e5f66c4e64356bd34d53d4e8202521d07595781
Roddy <rlm72@cam.ac.uk>
2016-12-02 14:51:34 +0000

    Added file reading for PatternDatas

--- Game/Pattern.cpp
+++ Game/Pattern.cpp
@@ -1,4 +1,6 @@
 #include "stdafx.h"
+#include <fstream>
+#include <sstream>
 #include "Pattern.h"
 #include "EntityManager.h"
 #include "StaticObj.h"
@@ -9,6 +11,45 @@ Pattern::Pattern(EntityManager* entity_manager, ResourceManager<sf::Texture, std
 }
 
 Pattern::~Pattern() = default;
+
+void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    // thanks StackOverflow
+	std::stringstream ss;
+	ss.str(s);
+	std::string item;
+	while (std::getline(ss, item, delim)) {
+		elems.push_back(item);
+	}
+}
+
+void Pattern::loadFile(std::filename) {
+	std:ifstream file(filename);
+	std::string str;
+	std::vector<std::string> parts;
+
+	while (std::getline(file, str)) {
+		std::vector<std::string> parts;
+		split(str, " ", parts);
+		std::string enemy_id = parts[0];
+		sfld::Vector2f position = Vector2f(std::stoi(parts[1]), std::stoi(parts[2]));
+		std::vector<Enemy::Action> actions;
+		for (int i = 2; i < parts.size(); i++) {
+			std::string w = parts[i];
+			if (w == "up") {
+				actions.push_back(Enemy::ACTION_UP);
+			} else if (w == "down") {
+				actions.push_back(Enemy::ACTION_DOWN);
+			} else if (w == "left") {
+				actions.push_back(Enemy::ACTION_LEFT);
+			} else if (w == "right") {
+				actions.push_back(Enemy::ACTION_RIGHT);
+			} else if (w == "shoot") {
+				actions.push_back(Enemy::ACTION_SHOOT);
+			}
+		}
+		addData(PatternData(actions, position, enemy_id));
+	}
+}
 
 void Pattern::addData(PatternData data) {
 	pattern_data_.push_back(data);
