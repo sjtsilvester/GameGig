af3d5750746d92798c24bed5a8e68ee2d9dd850c
pighe <pighe@DESKTOP-VMH6OUC>
2016-12-02 19:48:14 +0000

    death

--- Game/Player.h
+++ Game/Player.h
@@ -10,7 +10,9 @@ public:
 	void sfmlEvent(sf::Event evt);
 	void takeDamage(int amount);
 	int getHealth() const;
+	void render(sf::RenderTarget* target);
 private:
+	bool dead;
 	int timer_;
 	int reload_;
 
